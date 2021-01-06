4aac51902b832ad3506d72899fd513447d489a8d
Priya Srivastava <shivikapriya730@gmail.com>
2021-01-07 00:22:00 +0530

    Create Middle of the Linked List.java

---
+++ LinkedList/Middle of the Linked List.java
@@ -0,0 +1,22 @@
+/** ----------BRUTE FORCE------------
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode A[] = new ListNode[100];
+        int i = 0;
+         while(head != null){
+             A[i++]= head;
+             head = head.next;
+         }
+        
+        return A[i/2];
+    }
+}
